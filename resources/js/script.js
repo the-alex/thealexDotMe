02809a368cb4356c31dfd19767956758e6a6be64
the-alex <thealex@umich.edu>
2015-02-01 03:06:25 -0500

    Created first blog post
    Also added tabs.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,9 @@
-
-
-function genRandHobby() { 
+function genRandHobby() {
     var myHobbies = ["Photographer", "Amateur Designer", "Musician", "Nerd", "Maker", "Tinkerer"];
     var rando = Math.floor(Math.random() * myHobbies.length)
     var datSpan = document.getElementById("hobby");
     datSpan.innerHTML = myHobbies[rando];
 }
+
+
+genRandHobby();

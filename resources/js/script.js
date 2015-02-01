ec4e011302acef7d8ff6f3488cd3d2f0dec8b8a3
the-alex <thealex@umich.edu>
2015-02-01 08:15:01 -0500

    Added images and a gallery with lightbox modal

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,5 +5,19 @@ function genRandHobby() {
     datSpan.innerHTML = myHobbies[rando];
 }
 
+$(document).ready(function () {
+    $('li img').on('click', function () {
+        var src = $(this).attr('src');
+        var img = '<img src="' + src + '" class="img-responsive"/>';
+        $('#gallery-modal').modal();
+        $('#gallery-modal').on('shown.bs.modal', function () {
+            $('#gallery-modal .modal-body').html(img);
+        });
+        $('#gallery-modal').on('hidden.bs.modal', function () {
+            $('#gallery-modal .modal-body').html('');
+        });
+    });
+})
+
 
 genRandHobby();

93573a5686d5927491d8d83f073225a9422647bb
the-alex <thealex@umich.edu>
2015-02-02 23:53:42 -0500

    Resized images and optimized load times

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,8 +7,12 @@ function genRandHobby() {
 
 $(document).ready(function () {
     $('li img').on('click', function () {
-        var src = $(this).attr('src');
-        var img = '<img src="' + src + '" class="img-responsive"/>';
+        src = $(this).attr('src');
+        img_name = src.slice(src.indexOf("thumbnails/") + "thumbnails/".length, src.length);
+        img_name = img_name.slice(0, img_name.indexOf(".thumbnail"));
+        src = src.slice(0, src.indexOf("thumbnails"));
+        new_src = src + "hd/" + img_name;
+        var img = '<img src="' + new_src + '" class="img-responsive"/>';
         $('#gallery-modal').modal();
         $('#gallery-modal').on('shown.bs.modal', function () {
             $('#gallery-modal .modal-body').html(img);
